c0b6d7375034e62007d394dc81a0fbde9a72b707
gilberto <gilbertoguedes@gmail.com>
2019-09-10 01:37:38 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AuthenticationService } from './_services';
 import { User } from './_models';
 
+declare var $: any;
+
 @Component({ selector: 'app', templateUrl: 'app.component.html' })
-export class AppComponent {
+export class AppComponent implements OnInit{
     currentUser: User;
+
+    ngOnInit() {
+        $(document).ready(() => {
+            $("#menu-toggle").click(function(e) {
+                console.log('akiiii');
+                e.preventDefault();
+                $("#wrapper").toggleClass("toggled");
+            });
+        });
+        
+    }
 
     constructor(
         private router: Router,
