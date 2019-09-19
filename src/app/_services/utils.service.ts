a22b166ec8888ddb80a41c309dd101f605d64af5
gilbertoguedes <glbrt.gds@gmail.com>
2019-09-18 23:38:50 -0400

    Merge branch 'master' of https://github.com/emigort/iflower
    # Conflicts:
    #	README.md

---
+++ src/app/_services/utils.service.ts
@@ -0,0 +1,35 @@
+﻿import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+
+import { environment } from '@environments/environment';
+// import { User } from '@app/_models';
+
+@Injectable({ providedIn: 'root' })
+export class UtilsService {
+    private init_year = 2019;
+    constructor() { }
+
+    GetYears(initial_year, end_year) {
+        if (typeof initial_year === 'undefined') {
+            initial_year = this.init_year;
+        }
+        if (typeof end_year === 'undefined') {
+            end_year = this.GetCurrentYear();
+        }
+        console.dir([initial_year, end_year]);
+        return this.ArrayRange(initial_year, end_year);
+    }
+
+    GetCurrentYear () {
+        var currentdate = new Date();
+        return currentdate.getFullYear();
+    }
+
+    ArrayRange(start, end) {
+        var foo = [];
+        for (var i = start; i <= end; i++) {
+            foo.push(i);
+        }
+        return foo;
+    }
+}
