94bc314db5c7085629844340b1aca6bb7fdfca65
gilbertoguedes <glbrt.gds@gmail.com>
2019-09-30 11:14:51 -0400

    Component to show message at the end of registration process

---
+++ src/app/register-check-email-message/register-check-email-message.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-register-check-email-message',
+  templateUrl: './register-check-email-message.component.html',
+  styleUrls: ['./register-check-email-message.component.less']
+})
+export class RegisterCheckEmailMessageComponent implements OnInit {
+
+  private email:string;
+  constructor(private activateRoute: ActivatedRoute, private router: Router) { }
+
+  ngOnInit() {
+    document.body.classList.add('bg-login-img');
+
+    this.activateRoute.params.subscribe(params=>{
+      console.log("ke pinga e: "+ params.email)
+      this.email = params.email;
+    });
+  }
+
+}
