f31fdb27a93429238e32d01a364c53fc737794d7
gilberto <gilbertoguedes@gmail.com>
2019-10-02 10:18:41 -0400

    ed

---
+++ src/app/_services/router.service.ts
@@ -0,0 +1,25 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {retry, catchError} from 'rxjs/operators';
+
+import { environment } from '@environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class RouterService {
+    constructor() { }
+
+    private router_data: any = undefined;
+
+    public getRouterData()
+    {
+        return this.router_data;
+    }
+
+    public setRouterData(data:any)
+    {
+        return this.router_data = data;
+    }
+
+    
+}
