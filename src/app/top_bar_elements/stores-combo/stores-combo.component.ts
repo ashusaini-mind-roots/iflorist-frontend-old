e8501107a97556e8a09cb11d6f14d6fee13ba327
gilbertoguedes <glbrt.gds@gmail.com>
2019-10-12 13:54:24 -0400

    select options stores component

---
+++ src/app/top_bar_elements/stores-combo/stores-combo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stores-combo',
+  templateUrl: './stores-combo.component.html',
+  styleUrls: ['./stores-combo.component.less']
+})
+export class StoresComboComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
