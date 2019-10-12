681e5256f8996369b8d81ba51cd14cb9a048723d
gilberto <gilbertoguedes@gmail.com>
2019-10-12 09:36:12 -0400

    Styling marchant form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { UserService, AuthenticationService, PlanService } from './_services';
+import { UserService, AuthenticationService, PlanService, CompanyService } from './_services';
 /*import {PlanService} from '../_services'*/
 import { User } from './_models';
 
@@ -14,6 +14,7 @@ export class AppComponent implements OnInit{
     toggled: boolean = false;
 
     modules: any;
+    stores:any;
 
     ngOnInit() {
        /*$(document).ready(() => {
@@ -32,22 +33,33 @@ export class AppComponent implements OnInit{
             });
         })(jquery);*/
         this.loadModules();
+        this.loadStores();
 
     }
 
     loadModules(){
         const currentUser = this.authenticationService.currentUserValue;
-
         if(currentUser)
         {
-            console.log(currentUser.user.id);
+            //console.log(currentUser.user.id);
             return this.planService.getByUser(currentUser.user.id).subscribe((data: any) =>{
                 this.modules = data.modules;
                 console.log(data.modules);
             });
         }
+    }
 
-      }
+    loadStores(){
+        const currentUser = this.authenticationService.currentUserValue;
+        if(currentUser)
+        {
+            console.log(currentUser.user.id);
+            return this.companyService.storesByCompany().subscribe((data: any) =>{
+                this.stores = data.stores;
+                console.log(this.stores);
+            });
+        }
+    }
 
     loadComponents(component_id: string)
     {
@@ -67,6 +79,7 @@ export class AppComponent implements OnInit{
         private router: Router,
         private userService: UserService,
         private planService:PlanService,
+        private companyService:CompanyService,
         private authenticationService: AuthenticationService
     ) {
         this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
