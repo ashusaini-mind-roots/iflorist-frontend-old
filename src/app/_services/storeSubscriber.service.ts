7465c0882ae129618283dfc0b9acce7a2f0b5b1a
gilbertoguedes <glbrt.gds@gmail.com>
2019-10-15 17:10:27 -0400

    cost-of-fresh, used to implement generic receive of top nav bar store combobox change, and receive from year and square component.

---
+++ src/app/_services/storeSubscriber.service.ts
@@ -0,0 +1,19 @@
+﻿import { Injectable } from '@angular/core';
+import { MessageService } from "./message.service";
+import {Subscription} from "rxjs";
+
+@Injectable({ providedIn: 'root' })
+export class StoreSubscriberService{
+    subscription: Subscription;
+
+    constructor(private messageService: MessageService) { }
+
+    subscribe(reference, callback_function){
+        this.subscription = this.messageService.getMessage().subscribe(message => {
+            if (message) {
+                callback_function(reference,JSON.parse(localStorage.getItem('selectedStorage')))
+                // this.selectedStorage = JSON.parse(localStorage.getItem('selectedStorage'));
+                // this.receiveStorage(this.selectedStorage);
+            }});
+    }
+}
