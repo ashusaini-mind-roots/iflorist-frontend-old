4408012c689d0e6f58842e673eb401463b7901fb
gilbertoguedes <glbrt.gds@gmail.com>
2019-10-19 18:53:53 -0400

    week-panel component, empty

---
+++ src/app/week-panel/week-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-week-panel',
+  templateUrl: './week-panel.component.html',
+  styleUrls: ['./week-panel.component.less']
+})
+export class WeekPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
