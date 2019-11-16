9cf08eba86ec559ff5c6d2abf1b0de02b4061cdc
gilbertoguedes <glbrt.gds@gmail.com>
2019-11-15 21:25:07 -0500

    employee

---
+++ src/app/employees/employee-parent/employee-parent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-parent',
+  templateUrl: './employee-parent.component.html',
+  styleUrls: ['./employee-parent.component.less']
+})
+export class EmployeeParentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
