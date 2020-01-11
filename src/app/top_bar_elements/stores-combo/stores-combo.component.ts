0e519b04001913b3dac89fa13b33433ccbe3cfd6
gilbertoguedes <glbrt.gds@gmail.com>
2020-01-11 10:50:40 -0500

    Working on security

--- src/app/top_bar_elements/stores-combo/stores-combo.component.ts
+++ src/app/top_bar_elements/stores-combo/stores-combo.component.ts
@@ -24,12 +24,13 @@ export class StoresComboComponent implements OnInit {
   reloadData(){
     this.storeService.getStoreList().subscribe((data: any) =>{
       this.stores = data.stores;
-      if(this.stores.length > 0){
+      if(this.stores && this.stores.length > 0){
         this.storeIndexSelected = this.stores[0].id;
         this.populateSelectedStorage(this.stores[0]);
         // console.log(this.stores[0])
       }
-      // console.log(this.stores)
+      console.log("estoresss")
+       console.log(data)
     })
   }
 
