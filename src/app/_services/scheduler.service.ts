6b16c7a98751df05b6858fcf25532b209f060be1
gilbertoguedes <glbrt.gds@gmail.com>
2020-01-13 14:24:35 -0500

    Working on security

--- src/app/_services/scheduler.service.ts
+++ src/app/_services/scheduler.service.ts
@@ -27,43 +27,8 @@ export class SchedulerService{
         // console.log("cojone: " + `${environment.apiUrl}/schedule/all/${store_id}/${week_id}`)
         return this.http.get(`${environment.apiUrl}/schedule/all/${store_id}/${week_id}`);
     }
-
     updateOrAdd = function (year, week_id, schedule_days) {
         return this.http.post(`${environment.apiUrl}/schedule/update_or_add/`, {year, week_id, schedule_days} );
     }
-    
-    // getSevenDays(store_id, week_id): Observable<any> {
-  //   // console.log(`${environment.apiUrl}/store/all`);
-  //    return this.http.get(`${environment.apiUrl}/daily_revenue/seven_days_week/${store_id}/${week_id}`);
-  // }
-  // getWeeks(year): Observable<any> {
-  //   // console.log(`${environment.apiUrl}/store/all`);
-  //   return this.http.get(`${environment.apiUrl}/week/week_by_year/${year}`);
-  // }
-  // getInvoices(cost_of,store_id, week_id): Observable<any> {
-  //   return this.http.get(`${environment.apiUrl}/invoice/all/${cost_of}/${store_id}/${week_id}`);
-  // }
-  // createInvoice(cost_of, invoice_number,invoice_name,total,store_id,week_id) {
-  //   return this.http.post(`${environment.apiUrl}/invoice/create`,
-  //       {
-  //         "invoice_number":  invoice_number,
-  //         "invoice_name":  invoice_name,
-  //         "total":  total,
-  //         "store_id": store_id,
-  //         "week_id": week_id,
-  //         "cost_of": cost_of,
-  //       });
-  //
-  // }
-  // getProjWeeklyRev(store_id, week_id) : Observable<any> {
-  //   return this.http.get(`${environment.apiUrl}/weekly_projection_percent_revenue/proj_weekly_revenue/${store_id}/${week_id}`);
-  // }
-  // getTarget(cost_of) : Observable<any> {
-  //   return this.http.get(`${environment.apiUrl}/weekly_projection_percent_costs/target/${cost_of}`);
-  // }
-  //
-  // updateDay(id,merchandise,wire,delivery){
-  //     return this.http.put(`${environment.apiUrl}/daily_revenue/update/${id}`, {merchandise,wire,delivery} );
-  // }
 
 }
