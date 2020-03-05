f9362366e53b7b1144bb35850c5a542586ebfb13
gilberto <ajborgeslag@gmail.com>
2020-03-05 08:39:49 -0400

    u

--- src/app/companyemployee/companyemployee-list/companyemployee-list.component.ts
+++ src/app/companyemployee/companyemployee-list/companyemployee-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CompanyemployeeService } from "../../_services/companyemployee.service";
 import { UserService, AuthenticationService } from '@app/_services';
+import { MessageService } from "../../_services/message.service";
+import { environment } from '@environments/environment';
 
 @Component({
   selector: 'app-companyemployee-list',
@@ -10,16 +12,40 @@ import { UserService, AuthenticationService } from '@app/_services';
 export class CompanyemployeeListComponent implements OnInit {
   employees: any[];
   cols: any[];
+  displayList: boolean = true;
+  employeesDefault: any[];
+  urlImage: string = environment.apiUrl+'/companyemployee/show_imagen/';
 
   constructor(
       private userService: UserService,
       private authenticationService: AuthenticationService,
       private companyEmployeeService: CompanyemployeeService,
+	  private messageService: MessageService,
   ) { }
 
   ngOnInit() {
     this.loadHeaders();
     this.getEmployees();
+	
+	this.messageService.getChangeDisplayModeData().subscribe(message => {
+		console.log(message);
+		this.displayList = message;
+	});
+	
+	this.messageService.getFilterText().subscribe(message => {
+		this.employees = this.employeesDefault;
+		if(message)
+		{
+			let temp = [];
+			this.employees.forEach(obj=>{
+				if(obj.name.includes(message) || obj.email.includes(message)  || obj.phone_number.includes(message) || obj.status_name.includes(message))
+				{
+					temp.push(obj);
+				}
+			});
+			this.employees = temp;
+		}
+	});
   }
 
   getEmployees()
@@ -27,6 +53,7 @@ export class CompanyemployeeListComponent implements OnInit {
     console.log(this.authenticationService.currentUserValue.company.id);
     this.companyEmployeeService.getCompanyEmployees(this.authenticationService.currentUserValue.company.id).subscribe((response: any) =>{
       this.employees = response.employees;
+	  this.employeesDefault = this.employees;
       console.log("companyEmployees: ");
       console.log(this.employees);
     });
---
+++ src/app/companyemployee/companyemployee-parent/companyemployee-parent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-companyemployee-parent',
+  templateUrl: './companyemployee-parent.component.html',
+  styleUrls: ['./companyemployee-parent.component.less']
+})
+export class CompanyemployeeParentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
