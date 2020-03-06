8d411ce7593e9402af87649a81c7e9084ddbdcdb
gilbertoguedes <glbrt.gds@gmail.com>
2020-03-06 12:01:05 -0500

    working on scheduler calendar view

--- src/app/scheduler_elements/scheduler-calendar-view/scheduler-calendar-view.component.ts
+++ src/app/scheduler_elements/scheduler-calendar-view/scheduler-calendar-view.component.ts
@@ -18,14 +18,28 @@ export class SchedulerCalendarViewComponent implements OnInit {
   categoriesEmployeesListSelectedItem: any;
   employeesListSelected: any;
   employeesList: any[];
-  employeesScheduleList: any[];
-  employeeToShow: any;
+  employeesList_add_modal: any[];
+  employeesListSelected_add_modal: any;
+  employeesScheduleList: any[];//parse data from the server, the core of all the information here
+  // employeeToShow: any;
+
+  employeesToShow: any[];
+  seven_days_dates: any[];
+
   employee_scheduler_day_to_edit: any;
   visible_edit_day_modal: boolean = false;
+  visible_add_day_modal: boolean = false;
 
   time_in: any;
   time_out: any;
   break_time: any;
+
+  add_time_in: any;
+  add_time_out: any;
+  add_break_time: any;
+
+  add_week_day_number: any;
+  employee_scheduler_day_to_save: any;
 
   constructor(
       private schedulerService: SchedulerService,
@@ -35,7 +49,9 @@ export class SchedulerCalendarViewComponent implements OnInit {
     storeSubscriberService.subscribe(this,function (ref,store) {
       ref.receiveStorage(store);
     });
-    this.employeeToShow = {};
+    // this.employeeToShow = {};
+    this.employeesToShow = [];
+    this.seven_days_dates = [];
     this.employee_scheduler_day_to_edit = {};
   }
 
@@ -63,7 +79,8 @@ export class SchedulerCalendarViewComponent implements OnInit {
   }
 
   parseScheduleInformationResponse = function(categories_schedules,dates_of_week){
-    // console.log(categories_schedules)
+    console.log("categories schedules")
+     console.log(categories_schedules)
     var empScheList: any[] = [];
     for(var i = 0 ; i < categories_schedules.length ; i++){
       for(var j = 0 ; j < categories_schedules[i].employees.length ; j++){
@@ -72,6 +89,9 @@ export class SchedulerCalendarViewComponent implements OnInit {
           var schedul = categories_schedules[i].employees[j].schedule_days[k];
           schedul.date = dates_of_week[k];
           schedul.array_position = k;
+          schedul.employee_name = categories_schedules[i].employees[j].name;
+          schedul.category_name = categories_schedules[i].category_name;
+          schedul.employee_id = categories_schedules[i].employees[j].employee_id;
           if(schedul.time_in != undefined)
             schedul.time_in = this.utilService.getStringTimeFormat(new Date(schedul.time_in));
           if(schedul.time_out != undefined)
@@ -139,25 +159,41 @@ export class SchedulerCalendarViewComponent implements OnInit {
     for(let i=0 ; i<this.categoriesEmployeesList.length ; i++){
         if(this.categoriesEmployeesList[i].id == category_id ) {
           this.employeesList = this.categoriesEmployeesList[i].employees;
+          console.log("la pinga pa tol mundo")
+          console.log(this.employeesList);
+          this.employeesList_add_modal = this.employeesList.map(x => Object.assign([], x));
          // this.employeesListSelected = this.categoriesEmployeesList[i].employees[0].id;
-         // break;
+          break;
         }
     }
+    console.log("employeesList_add_modal")
+    console.log(this.employeesList_add_modal)
   }
 
   populateEmployeeToShow = function(category_id, employee_id){
    // if(this.employeesScheduleList.length > 0){
    //  console.log(this.employeesScheduleList)
-     for (let i = 0 ; i < this.employeesScheduleList.length ; i++){
-        let employee = this.employeesScheduleList[i];
-        if(employee.employee_id == employee_id && employee.category_id ==  category_id){
-            this.employeeToShow = employee;
-            break;
-        }
-     }
-   // }
-   console.log("Employee to show");
-   console.log(this.employeeToShow);
+  //  employee_id = -1;//esto es probando, en realidad se pasa por parametro, ver como poner en el combo de los employees la primera option en -1 el value
+    if(employee_id == -1){//show all employees
+      this.employeesToShow = this.employeesScheduleList;
+    }
+    else {
+       this.employeesToShow = [];
+       for (let i = 0 ; i < this.employeesScheduleList.length ; i++){
+          let employee = this.employeesScheduleList[i];
+          if(employee.employee_id == employee_id && employee.category_id ==  category_id){
+              this.employeeToShow = employee;
+              this.employeesToShow.push(employee);
+              break;
+          }
+       }
+    }
+    if(this.employeesToShow.length > 0)
+      this.seven_days_dates = this.employeesToShow[0].schedule_days;
+
+   // console.log("Employee to show");
+   // console.log(this.employeeToShow);
+   //  console.log(this.employeesToShow);
   }
 
   showEdit_day_modal = function(day, time_in, time_out, break_time){
@@ -180,17 +216,23 @@ export class SchedulerCalendarViewComponent implements OnInit {
   }
 
   editSchedule = function(){
+    console.log("empl to ed")
+    console.log(this.employee_scheduler_day_to_edit);
     if(this.employee_scheduler_day_to_edit){
       this.employee_scheduler_day_to_edit.time_in = this.time_in;
       this.employee_scheduler_day_to_edit.time_out = this.time_out;
       this.employee_scheduler_day_to_edit.break_time = this.break_time;
-      let schedule_days = this.getSchedule_days(this.categoriesEmployeesListSelectedItem,this.employeesListSelected);
-      if(schedule_days != undefined && this.employeeToShow){
-        this.updateSchedulesByCategory(schedule_days,this.employeeToShow.category_name, this.employeeToShow.employee_id);
+      console.log(this.employee_scheduler_day_to_edit);
+      let schedule_days = this.getSchedule_days(this.categoriesEmployeesListSelectedItem,this.employee_scheduler_day_to_edit.employee_id);
+      console.log(schedule_days)
+      if(schedule_days != undefined && this.employee_scheduler_day_to_edit){
+        this.updateSchedulesByCategory(schedule_days,this.employee_scheduler_day_to_edit.category_name, this.employee_scheduler_day_to_edit.employee_id);
       }
     }
     this.visible_edit_day_modal = false;
   }
+
+
 
   updateSchedulesByCategory = function(schedule_days,category_name,employee_id){
     var esw_array = new Array();
@@ -242,5 +284,98 @@ export class SchedulerCalendarViewComponent implements OnInit {
       }
     }
     console.log(this.employeesScheduleList)
+  }
+
+  getDays(weekDay){
+    let days: any[] = [];
+    if(this.employeesToShow.length > 0){
+        for (let i = 0 ; i < this.employeesToShow.length ; i++){
+          days.push(this.employeesToShow[i].schedule_days[weekDay]);
+        }
+    }
+    // console.log("days")
+    // console.log(days)
+    // console.log(this.seven_days_dates.length)
+    return days;
+  }
+
+  hasShift(weekDay){
+    if(this.employeesToShow.length > 0){
+      for (let i = 0 ; i < this.employeesToShow.length ; i++){
+        if(this.employeesToShow[i].schedule_days[weekDay].time_in)
+          return this.employeesToShow[i].schedule_days[weekDay];
+      }
+    }
+    return undefined;
+  }
+
+  showAdd_day_modal(week_day_number){
+    this.visible_add_day_modal = true;
+    this.add_week_day_number = week_day_number;
+    this.add_time_in = undefined;
+    this.add_time_out = undefined;
+    this.add_break_time = undefined;
+  }
+
+  saveSchedule = function(){
+    let employee_scheduler_day_to_save = undefined;
+    // employeesListSelected_add_modal
+
+    this.setShiftToDayToSaveSchedule(this.add_time_in, this.add_time_out, this.add_break_time);
+
+
+      // console.log(this.employee_scheduler_day_to_edit);
+      let schedule_days = this.getSchedule_days(this.categoriesEmployeesListSelectedItem,this.employeesListSelected_add_modal);
+      console.log("mojon divino")
+      console.log(schedule_days)
+    console.log("scheduler day to save")
+    console.log(this.employee_scheduler_day_to_save)
+      if(schedule_days != undefined && this.employee_scheduler_day_to_save){
+        this.updateSchedulesByCategory(schedule_days,this.employee_scheduler_day_to_save.category_name, this.employee_scheduler_day_to_save.employee_id);
+      }
+
+    this.visible_edit_day_modal = false;
+  }
+
+  setShiftToDayToSaveSchedule(add_time_in,add_time_out,add_break_time){
+    this.employee_scheduler_day_to_save = undefined;
+      for (let i = 0 ; i < this.employeesScheduleList.length ; i++){
+          if(this.employeesScheduleList[i].employee_id == this.employeesListSelected_add_modal){
+            for (let j = 0 ; j < this.employeesScheduleList[i].schedule_days.length ; j++){
+                if(this.employeesScheduleList[i].schedule_days[j].array_position == this.add_week_day_number){
+                  this.employeesScheduleList[i].schedule_days[j].time_in = add_time_in;
+                  this.employeesScheduleList[i].schedule_days[j].time_out = add_time_out;
+                  this.employeesScheduleList[i].schedule_days[j].break_time = add_break_time;
+                  this.employee_scheduler_day_to_save = this.employeesScheduleList[i].schedule_days[j];
+                  break;
+                }
+            }
+          }
+      }
+  }
+
+  getDayByEmployeeId_weekDay_number(employee_id,week_day_number){
+      for (let i = 0 ; i < this.employeesScheduleList.length ; i++){
+        if(this.employeesScheduleList[i].employee_id == this.employeesListSelected_add_modal){
+          for (let j = 0 ; j < this.employeesScheduleList[i].schedule_days.length ; j++){
+            if(this.employeesScheduleList[i].schedule_days[j].array_position == this.add_week_day_number){
+              return this.employeesScheduleList[i].schedule_days[j];
+              break;
+            }
+          }
+        }
+      }
+      return undefined;
+  }
+
+  employeesListSelected_add_modal_change(event: any){
+    let day = this.getDayByEmployeeId_weekDay_number(event.target.value,this.add_week_day_number);
+    if(day){
+      this.add_time_in = day.time_in;
+      this.add_time_out = day.time_out;
+      this.add_break_time = day.break_time;
+    }
+    console.log(day);
+
   }
 }
